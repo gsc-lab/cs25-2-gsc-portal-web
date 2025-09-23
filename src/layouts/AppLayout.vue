8f4ae68be80ec7227f03101560ae2d066f18c159
ohiayame <0819aym001@gmail.com>
2025-09-23 10:15:39 +0900

    ✨ feat: Layout, header 구조 추가

---
+++ src/layouts/AppLayout.vue
@@ -0,0 +1,23 @@
+<script setup>
+import Header from "@/layouts/MainHeader.vue";
+import { ref, watchEffect } from "vue";
+
+// 페이지 타이틀 받아오기
+const { pageName } = defineProps({pageName: { type: String },})
+const title = ref("")
+
+// title 추적
+watchEffect(() => {
+  title.value  = pageName
+  console.log("slot:", pageName);
+})
+
+</script>
+
+<template>
+    <Header />
+    <main>
+      <h1>{{ title }}</h1>
+      <slot></slot>
+    </main>
+</template>
---
+++ src/layouts/MainHeader.vue
@@ -0,0 +1,5 @@
+<template>
+  <header>
+    <h1>GSC-Portal</h1>
+  </header>
+</template>
