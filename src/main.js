db6ec341a20461cec70cf7633e4cda2bd27cd6ee
ohiayame <0819aym001@gmail.com>
2025-09-28 23:06:47 +0900

    ✨ feat: pinia 설정 추가

--- src/main.js
+++ src/main.js
@@ -3,10 +3,11 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from '@/router/index'
 import './styles/index.css'
+import pinia from '@/stores'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.use(pinia)
 app.mount('#app')
