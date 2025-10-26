30aead8dc68c5b7252dc1ce25d6878f01f871538
ohiayame <0819aym001@gmail.com>
2025-10-26 17:57:13 +0900

    ✨ feat: 강의실 등록 + 신청  추가

--- src/api/classroomApi.js
+++ src/api/classroomApi.js
@@ -37,13 +37,17 @@ export const getClassroomsInfo = async () => {
 
 
 // ---------------------------------------------------------------
-// 교실 정보 조회 -> 강의실에서 사용
-//     [{
-//      "classroom_id": "101",
-//      "building": "본관",
-//      "room_number": "101",
-//      "room_type": "강의실"
-//    },]
+// 교실 예약 정보
+//     {
+//       SUN: [{
+//      "id": "1"
+//      "classroom": "CR001",
+//      "date": "2000-00-00",
+//      "start": "00:00:00",
+//      "end": "00:00:00",
+//      "user": "홍길동"
+//      },{}]
+//    }
 // ---------------------------------------------------------------
 export const getReservation = async (classroom_id) => {
   try{
@@ -58,6 +62,33 @@ export const getReservation = async (classroom_id) => {
 }
 
 
+// ---------------------------------------------------------------
+// 주말 투표 현황 데이터
+//   [{
+//      "poll_id": "P001",
+//      "grade_name": "1학년",
+//      "poll_date": "2025-10-17",
+//      "day_of_week": "토요일",
+//      "required_count": 8,
+//      "vote_count": 4,
+//      "is_opened": false,
+//      "user_voted": true
+//     },{}]
+// ---------------------------------------------------------------
+export const getPoll = async (date) => {
+  console.log("getPoll: " , date);
+  try{
+    const res = await api.get(`/classrooms/polls`, {
+      params: {
+        date: "2025-04-02" // date
+      }
+    })
+    console.log(res.data);
+    return res.data;
+  } catch (e) {
+    errorMsg(e);
+  }
+}
 
 
 // ========================== POST ===============================
@@ -71,6 +102,62 @@ export const postReservation = async (reservationData) => {
       reserve_date: reservationData.reserve_date,
       start_time: reservationData.start_time,
       end_time: reservationData.end_time
+    })
+    // console.log(res.data);
+    return res.data;
+  } catch (e) {
+    errorMsg(e);
+  }
+}
+
+// ---------------------------------------------------------------
+// 강의실 등록
+// ---------------------------------------------------------------
+export const postClassroom = async (CRdata) => {
+  try{
+    const res = await api.post(`/classrooms`, {
+      building: CRdata.building,
+      room_number: CRdata.room_number,
+      room_type: CRdata.room_type,
+
+      // mode: CRdata.mode,
+      // limit: CRdata.limit,
+    })
+    // console.log(res.data);
+    return res.data;
+  } catch (e) {
+    errorMsg(e);
+  }
+}
+
+// ---------------------------------------------------------------
+// 신청 등록 / 취소
+// ---------------------------------------------------------------
+export const postPoll = async (poll_id, action) => {
+  try{
+    const res = await api.post(`/classrooms/polls/${poll_id}/vote`, {
+      action: action,
+    })
+    // console.log(res.data);
+    return res.data;
+  } catch (e) {
+    errorMsg(e);
+  }
+}
+
+// ========================== patch ===============================
+// ---------------------------------------------------------------
+// 강의실 수정
+// ---------------------------------------------------------------
+export const patchClassroom = async (CRdata) => {
+  try{
+    const res = await api.patch(`/classrooms`, {
+      building: CRdata.building,
+      room_number: CRdata.room_number,
+      room_type: CRdata.room_type,
+
+      // mode: CRdata.mode,
+      // limit: CRdata.limit,
     })
     // console.log(res.data);
     return res.data;
--- src/pages/Classroom/AttendancePoll.vue
+++ src/pages/Classroom/AttendancePoll.vue
@@ -1,6 +1,61 @@
 <script setup>
+import {ref, watch} from 'vue'
+import { getPoll, postPoll } from '@/api/classroomApi';
+import Setting from './Sidebar/components/Setting.vue';
+
+const pollData = ref()     //해당 주 신청 데이터
+const today = new Date()
+const selectDate = ref(today)
+const isOpen = ref(false)
+// ====================================================
+watch(() => selectDate.value, async () => {
+  pollData.value = await getPoll(selectDate.value.toISOString().split('T')[0])
+}, { immediate: true })
+// 지난주
+const handleBefore = () => {
+  selectDate.value = new Date(selectDate.value.setDate(selectDate.value.getDate() - 7))
+}
+// 다음주
+const handleAfter = () => {
+  selectDate.value = new Date(selectDate.value.setDate(selectDate.value.getDate() + 7))
+}
+
+// 신청 / 취소
+const handleAction = async (poll_id, isVoted) => {
+  const action = isVoted ? "cancel" : "apply"
+  await postPoll(poll_id, action)  // api전송
+  pollData.value = await getPoll() // 신청 데이터 갱신
+}
 </script>
 
 <template>
+  <div style="background-color:khaki;">
   AttendancePoll
+
+   <div>
+      <button @click="handleBefore">지난주</button>
+      <p>{{selectDate.toISOString().split('T')[0] }}</p>
+      <button @click="handleAfter">다음주</button>
+    </div>
+
+    <button @click="isOpen = !isOpen">설정</button>
+    <div v-if="isOpen">
+      <Setting />
+    </div>
+
+  <div v-for="data in pollData" style="background-color:aliceblue; margin: 3px;">
+    <p>{{ data.grade_name }}</p>
+    <p>{{ data.poll_date}} ( {{ data.day_of_week }} )</p>
+    <p>제한 : {{ data.required_count }}</p>
+    <p>현제 신청자 수: {{ data.vote_count }}</p>
+
+    <div>
+    <button @click="handleAction(data.poll_id, data.user_voted)">
+      <p v-if="data.user_voted">취소</p>
+      <p v-else>신청</p>
+    </button>
+    </div>
+
+  </div>
+  </div>
 </template>
