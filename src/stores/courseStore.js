900db0933b32ed14b76d97b0516d1094bbff95b2
Kwon_Hyuk_il <gurdlf320@naver.com>
2025-10-28 23:45:53 +0900

    ✨ feat: 과목 API 요청 및 store 관리

--- src/stores/courseStore.js
+++ src/stores/courseStore.js
@@ -1,34 +1,52 @@
 import { defineStore } from "pinia";
 import { getCourse } from "@/api/apiCourse";
+import { ref, computed } from "vue";
 
-export const useCourseStore = defineStore('course', {
-  state: () => ({
-    courses: [],
-    isLoading: false,
-    error: null,
+export const useCourseStore = defineStore('course', () => {
 
-    course_type: [
-      { course_type: 'general' },
-      { course_type: 'regular' },
-      { course_type: 'special' },
-      { course_type: 'korean' },
-    ],
-  }),
+  // 전체 과목 정보
+  const courses = ref([])
 
-  actions: {
+  const course_type = ref([
+    { course_type: 'general' },
+    { course_type: 'regular' },
+    { course_type: 'special' },
+    { course_type: 'korean' }
+  ])
 
-    async fetchCourse() {
-      this.isLoading = true
+  // 상태확인, 에러 메시지 저장
+  const isLoading = ref(false)
+  const error = ref('')
 
-      try {
-        const response = await getCourse()
-        this.courses = response
-      } catch (err) {
-        this.error = "과목 조회를 실패했습니다."
-        console.error(err)
-      } finally {
-        this.isLoading = false
-      }
+  const courseMap = computed(() => {
+    const map = new Map()
+
+    for (const course of courses.value) {
+      map.set(course.course_id, course)
     }
+    return map
+  })
+
+
+
+  async function fetchCourse() {
+    isLoading.value = true
+
+    try {
+      const response = await getCourse()
+      courses.value = response
+    } catch (err) {
+      error.value = err
+      console.error("전체 과목 정보 불러오기 실패", err)
+    } finally {
+      isLoading.value = false
+    }
+  }
+
+  return {
+    courses,
+    courseMap,
+    fetchCourse,
+    course_type,
   }
 })
