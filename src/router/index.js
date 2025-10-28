c94dd2df1e65181fb815b6f9156a7e59ea4201c7
Kwon_Hyuk_il <gurdlf320@naver.com>
2025-10-28 23:54:34 +0900

    ✨ feat: Router 추가

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,26 @@
 import { createWebHistory, createRouter } from 'vue-router'
-import MainPage from '@/pages/Main/MainPage.vue'
+import DashBoard from '@/pages/Main/DashBoard.vue'
 import NoticePage from '@/pages/Notice/NoticePage.vue'
 import TimeTablePage from '@/pages/TimeTable/TimeTablePage.vue'
 import NoticeView from '@/pages/Notice/NoticeView.vue'
 import RegisterPage from '@/pages/Register/RegisterPage.vue'
+import NoticeWrite from '@/pages/Notice/NoticeWrite.vue'
+import LoginPage from '@/pages/Login/LoginPage.vue'
+import CleaningPage from '@/pages/Cleaning/CleaningPage.vue'
+import NoticeEdit from '@/pages/Notice/NoticeEdit.vue'
+import SignupWaitPage from '@/pages/Register/SignupWaitPage.vue'
 
 const routes = [
-  { path: '/', component: MainPage },
-  { path: '/register', component: RegisterPage},
+  { path: '/login', component: LoginPage },
+  { path: '/dashboard', component: DashBoard },
+  { path: '/register', component: RegisterPage },
+  { path: '/registerWait', component: SignupWaitPage },
   { path: '/notice', component: NoticePage },
-  { path: '/notice/:notice_id', name: 'notice', component: NoticeView },
+  { path: '/notice/write', component: NoticeWrite },
+  { path: '/noticeView/:id', component: NoticeView },
+  { path: '/noticeEdit/:id', component: NoticeEdit },
   { path: '/timetable', component: TimeTablePage },
+  { path: '/cleaning', component: CleaningPage },
 ]
 
 export const router = createRouter({
