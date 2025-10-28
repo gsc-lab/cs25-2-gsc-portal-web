2c9364f47019539aec389b2866ae70bc49e2ef1a
Kwon_Hyuk_il <gurdlf320@naver.com>
2025-10-29 00:29:22 +0900

    Reapply "✨ feat: 청소 스타일링 추가 및 청소 날짜 동적으로  계산"
    This reverts commit 5ff2ba739d51912e09a8cf5d05d21fea56809d98.

--- src/router/index.js
+++ src/router/index.js
@@ -3,10 +3,23 @@ import { createRouter, createWebHistory } from "vue-router";
 import LoginPage from "@/pages/Login/LoginPage.vue";
 
 const routes = [
-  { path: "/", name: "LoginPage", component: LoginPage },
-  { path: "/cleaning-rosters", name: "CleaningRosterPage", component: () => import("@/pages/Cleaning/CleaningPage.vue") },
-  { path: "/cleaning-rosters/generator", name: "CleaningGeneratorPage", component: () => import("@/pages/Cleaning/components/CleaningPost.vue")}
-];
+  { path: '/', name: 'LoginPage', component: LoginPage },
+  {
+    path: '/cleaning-rosters',
+    name: 'CleaningRosterPage',
+    component: () => import('@/pages/Cleaning/CleaningPage.vue'),
+  },
+  {
+    path: '/cleaning-rosters/generator',
+    name: 'CleaningGeneratorPage',
+    component: () => import('@/pages/Cleaning/components/CleaningPost.vue'),
+  },
+  {
+    path: '/cleaning-rosters/delete',
+    name: 'CleaningDeletePage',
+    component: () => import('@/pages/Cleaning/components/CleaningDelete.vue'),
+  },
+]
 
 const router = createRouter({
   history: createWebHistory(),
