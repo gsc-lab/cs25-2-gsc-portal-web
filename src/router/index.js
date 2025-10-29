2d68af5972f2e46f5975264a515f5f8353d91ac8
Kwon_Hyuk_il <gurdlf320@naver.com>
2025-10-30 00:00:50 +0900

    ♻️ refactor: router 리팩토링

--- src/router/index.js
+++ src/router/index.js
@@ -1,46 +1,28 @@
 import { createWebHistory, createRouter } from 'vue-router'
-import DashBoard from '@/pages/Main/DashBoard.vue'
-import NoticePage from '@/pages/Notice/NoticePage.vue'
-import TimeTablePage from '@/pages/TimeTable/TimeTablePage.vue'
-import NoticeView from '@/pages/Notice/NoticeView.vue'
-import RegisterPage from '@/pages/Register/RegisterPage.vue'
-import NoticeWrite from '@/pages/Notice/NoticeWrite.vue'
-import LoginPage from '@/pages/Login/LoginPage.vue'
-import CleaningPage from '@/pages/Cleaning/CleaningPage.vue'
-import NoticeEdit from '@/pages/Notice/NoticeEdit.vue'
-import SignupWaitPage from '@/pages/Register/SignupWaitPage.vue'
 
 const routes = [
-  { path: '/login', component: LoginPage },
-  { path: '/dashboard', component: DashBoard },
-  { path: '/register', component: RegisterPage },
-  { path: '/registerWait', component: SignupWaitPage },
-  { path: '/notice', component: NoticePage },
-  { path: '/notice/write', component: NoticeWrite },
-  { path: '/noticeView/:id', component: NoticeView },
-  { path: '/noticeEdit/:id', component: NoticeEdit },
-  { path: '/timetable', component: TimeTablePage },
-  { path: '/cleaning', component: CleaningPage },
-  {
-    path: '/cleaning-rosters',
-    name: 'CleaningRosterPage',
-    component: () => import('@/pages/Cleaning/CleaningPage.vue'),
-  },
-  {
-    path: '/cleaning-rosters/generator',
-    name: 'CleaningGeneratorPage',
-    component: () => import('@/pages/Cleaning/components/CleaningPost.vue'),
-  },
-  {
-    path: '/cleaning-rosters/delete',
-    name: 'CleaningDeletePage',
-    component: () => import('@/pages/Cleaning/components/CleaningDelete.vue'),
-  },
+  { path: '/login', name: 'login', component: () => import('@/pages/Login/LoginPage.vue') },
+  { path: '/dashboard', name: 'dashboard', component: () => import('@/pages/Main/DashBoard.vue') },
+  { path: '/register', name: 'register', component: () => import('@/pages/Register/RegisterPage.vue') },
+  { path: '/registerWait', name: 'registerWait', component: () => import('@/pages/Register/SignupWaitPage.vue') },
+  { path: '/notice', name: 'notice', component: () => import('@/pages/Notice/NoticePage.vue') },
+  { path: '/notice/write', name: 'notice/write', component: () => import('@/pages/Notice/NoticeWrite.vue') },
+  { path: '/noticeView/:id', name: 'noticeView/:id', component: () => import('@/pages/Notice/NoticeView.vue') },
+  { path: '/noticeEdit/:id', name: 'noticeEdit/:id', component: () => import('@/pages/Notice/NoticeEdit.vue') },
+  { path: '/timetable', name: 'timetable', component: () => import('@/pages/TimeTable/TimeTablePage.vue') },
+  { path: '/cleaning', name: 'cleaning', component: () => import('@/pages/Cleaning/CleaningPage.vue') },
+  { path: '/cleaning-rosters', name: 'CleaningRosterPage', component: () => import('@/pages/Cleaning/CleaningPage.vue') },
+  { path: '/cleaning-rosters/generator', name: 'CleaningGeneratorPage', component: () => import('@/pages/Cleaning/components/CleaningPost.vue') },
+  { path: '/cleaning-rosters/delete', name: 'CleaningDeletePage', component: () => import('@/pages/Cleaning/components/CleaningDelete.vue') },
+  // { path: '/profile', name: 'profile', component }
+  { path: '/profile', name: 'profile', component: () => import('@/pages/Profile/Userinfo.vue') }
 ]
 
 export const router = createRouter({
   history: createWebHistory(),
   routes,
 })
+
+// router.beforeEach((to, from))
 
 export default router;
