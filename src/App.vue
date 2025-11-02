aef62840637161015e334e1cf6b9d3f121af9107
Kwon_Hyuk_il <gurdlf320@naver.com>
2025-11-03 02:04:59 +0900

    ♻️ refactor: 사용자 로그인 상태 확인 ( Header 표시 여부 추가 )

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,13 @@
 <template>
-  <MainHeader />
-  <router-view />
+  <div v-if="user.userInfo">
+    <MainHeader />
+  </div>
+  <router-view :key="$route.fullPath" />
 </template>
 
 <script setup>
 import MainHeader from './layouts/MainHeader.vue'
+import { useUserStore } from './stores/user'
+
+const user = useUserStore()
 </script>
