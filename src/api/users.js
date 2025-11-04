69262e9914c6dba4a93a5dea91cd827c00231a1c
Kwon_Hyuk_il <gurdlf320@naver.com>
2025-11-04 23:23:57 +0900

    📚 docs: 학생 정보 요청 ( api 요청 조건 )

--- src/api/users.js
+++ src/api/users.js
@@ -1,5 +1,6 @@
 import apiClient from "./index"
 
+// 모든 학생 정보 요청 -> 현재 관리자만 가능 -> 교수도 되도록 변경해야할듯
 export const getAllUser = async () => {
   const user = await apiClient.get('/admin/students')
   return user
