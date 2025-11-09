541ac6f94379b3df7d8eeb75d486c5ecbdeab48f
ohiayame <0819aym001@gmail.com>
2025-11-10 01:05:18 +0900

    ✨ feat: 실제 학생 정보로 등록, 학기 추가

--- src/pages/TimeTable/Sidebar/components/FukaPage.vue
+++ src/pages/TimeTable/Sidebar/components/FukaPage.vue
@@ -1,8 +1,9 @@
 <script setup>
 import StudentsSelect from '@/layouts/StudentsSelect.vue'
-import { getUsers } from '@/api/Data'
+import { getUserInfo } from '@/api/adminApi'
+import { getSections, postFukaStudents, postFukaCustomStudents } from '@/api/timetableApi'
 import { setTarget } from '@/utils/reName'
-import { ref, watch } from 'vue'
+import { onMounted, ref, watch } from 'vue'
 import { useTimetableStore } from '@/stores/timetable'
 
 const Tstore = useTimetableStore() // 시간표 store
@@ -13,32 +14,41 @@ const days = ['월', '화', '수', '목', '금']
 const enDays = ['MON', 'TUE', 'WED', 'THU', 'FRI']
 
 // api
-const users = getUsers() // 학생 명단
+const users = ref() // 학생 명단
 const selectedUsers = ref([]) // 선택 학생 정보 저장
+const sections = ref(null)
+const postFukaData = ref()
 
 // 값 저장
-const postFukaData = ref({
-  date: null,
-  day: null,
-  room: '정보관 403',
-  startTime: null,
-  endTime: null,
-  students: selectedUsers.value._rawValue,
+const setVal = () => {
+  postFukaData.value = {
+    section: null,
+    date: null, // 날짜
+    day: null, // 요일
+    room: '정보관 403',
+    startTime: null,
+    endTime: null,
+    student_ids: [],
+  }
+}
+
+onMounted(async () => {
+  sections.value = await getSections()
+  setVal()
+  // 재학생만 조회
+  users.value = await getUserInfo(undefined, 'enrolled')
 })
 
 // ================================= 학생 선택 =================================
 // 학생 id를 받고 selectedUsers에 저장
 const handleSelect = (id) => {
-  const selectedUser = users.filter((user) => user.user_id == id)
-  // console.log("selected", selectedUser[0]);
-  // console.log(selectedUsers.value);
+  const selectedUser = users.value.filter((user) => user.user_id == id)
   selectedUsers.value.push(selectedUser[0])
 }
 
 // 학생 선택 취소
 const handleDelete = (id) => {
   selectedUsers.value = selectedUsers.value.filter((user) => user.user_id != id)
-  // console.log(selectedUsers.value);
 }
 
 // ================================= store 감시 =================================
@@ -54,6 +64,7 @@ watch(
       postFukaData.value = {
         date: timetableData.date,
         day: timetableData.day,
+        room: '정보관 403',
         startTime: timetableData.startTime,
         endTime: timetableData.endTime,
       }
@@ -66,8 +77,27 @@ watch(
 
 // ================================= 등록 =================================
 // 저장버튼 누른 후 실행
-const handleSubmit = () => {
+const handleSubmit = async () => {
+  // student_ids에는 id만 저장
+  postFukaData.value.student_ids = selectedUsers.value.map((std) => std.user_id)
+  // 저장
+  if (selectType.value == 'default') await postFukaStudents(postFukaData.value)
+  // pick면 학기를 계산해서 저장
+  else {
+    // 학기 검색
+    const section = sections.value.find(
+      (section) =>
+        section.start_date <= postFukaData.value.date &&
+        section.end_date >= postFukaData.value.date,
+    )
+    postFukaData.value.section = section.sec_id // 학기 id 대입
+    await postFukaCustomStudents(postFukaData.value)
+  }
   console.log(postFukaData.value)
+  // 초기화
+  setVal()
+  await Tstore.setTimetable()
+  selectedUsers.value = []
 }
 </script>
 
@@ -84,10 +114,20 @@ const handleSubmit = () => {
 
   <!-- 요일 설정 -->
   <div v-if="selectType == 'default'">
-    <label for="day">요일 :</label>
-    <select id="day" v-model="postFukaData.day">
-      <option v-for="(day, idx) in days" :value="enDays[idx]">{{ day }}요일</option>
-    </select>
+    <div>
+      <label for="section">학기:</label>
+      <select id="section" v-model="postFukaData.section">
+        <option v-for="section in sections" :value="section.sec_id" :key="section">
+          {{ section.sec_id }}
+        </option>
+      </select>
+    </div>
+    <div>
+      <label for="day">요일 :</label>
+      <select id="day" v-model="postFukaData.day">
+        <option v-for="(day, idx) in days" :value="enDays[idx]" :key="idx">{{ day }}요일</option>
+      </select>
+    </div>
   </div>
   <div v-else>
     <label for="date">날짜 :</label>
@@ -104,11 +144,11 @@ const handleSubmit = () => {
   <div>
     <label for="time">교시 :</label>
     <select id="time" v-model="postFukaData.startTime">
-      <option v-for="startT in 12" :value="String(startT)">{{ startT }}</option>
+      <option v-for="startT in 12" :value="String(startT)" :key="startT">{{ startT }}</option>
     </select>
     ~
     <select id="time" v-model="postFukaData.endTime">
-      <option v-for="endT in 12" :value="String(endT)">{{ endT }}</option>
+      <option v-for="endT in 12" :value="String(endT)" :key="endT">{{ endT }}</option>
     </select>
     교시
   </div>
