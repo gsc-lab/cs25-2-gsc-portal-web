91723f1bceccc5af43f45ca834cf72bb5aafcc0f
ohiayame <0819aym001@gmail.com>
2025-11-10 00:56:20 +0900

    🐛 fix: 정의 오류 수정

--- src/stores/timetable.js
+++ src/stores/timetable.js
@@ -37,7 +37,7 @@ export const useTimetableStore = defineStore('timetable', {
     // ------------------------ adminTimetable --------------------------
     // [ set ] : Timetable 셋팅
     async setTimetable(day = this.selectTT.date) {
-      if (day == null) new Date().toISOString().split('T')[0]
+      if (day == null) day = new Date().toISOString().split('T')[0]
       this.adminTimetable = await getAdminTimetable(day)
       this.date = day
       console.log('store: adminTimetable', this.adminTimetable)
