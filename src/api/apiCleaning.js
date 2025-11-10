b48e137617da12657e1f65a37daffe1bf8fc71ac
Kwon_Hyuk_il <gurdlf320@naver.com>
2025-11-10 20:42:43 +0900

    ✨ feat: 청소 관련 필요 API 추가

--- src/api/apiCleaning.js
+++ src/api/apiCleaning.js
@@ -1,6 +1,6 @@
 import apiClient from "./index";
 
-// 청소 당번 호출 API
+// 청소 당번 주간 호출 API
 // formData : 학년 정보, 날짜 정보 ( 학년 정보가 없을 경우에는 전체 호출 )
 export const getCleaningRoster = async (formData) => {
   const cleaningRoster = await apiClient.get('/cleaning-rosters', {
@@ -9,6 +9,14 @@ export const getCleaningRoster = async (formData) => {
 
   return cleaningRoster.data
 }
+
+// 청소 당번 한달 호출 API (monthly)
+export const getCleaningRosterMonthly = async () => {
+  const response = await apiClient.get('/cleaning-rosters/monthly')
+
+  return response.data
+}
+
 
 // 학기 목록 조회
 export const getSections = async () => {
@@ -22,4 +30,12 @@ export const getClassRooms = async () => {
   const classRooms = await apiClient.get('/modal/common/classrooms')
 
   return classRooms.data
+}
+
+// 청소 로스터 추가
+export const postCleaningRoster = async (roster) => {
+  const createRoster = await apiClient.post('/cleaning-rosters/generate', roster, {
+    headers: { 'Content-Type': 'application/json' }
+  })
+  return createRoster.data
 }
