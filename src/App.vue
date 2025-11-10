a331852e0f9fb552300db6fd4d542a53c6a33dbf
Kwon_Hyuk_il <gurdlf320@naver.com>
2025-11-10 23:40:25 +0900

    🐛 fix: 불필요 알고리즘 제거

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,3 @@
 <template>
-  <MainHeader v-if="user.userInfo" />
   <router-view />
 </template>
-
-<script setup>
-import MainHeader from './layouts/MainHeader.vue'
-import { useUserStore } from './stores/user'
-
-const user = useUserStore()
-</script>
