ce9f92a4894e980d13e5cce271565932af6dc181
Kwon_Hyuk_il <gurdlf320@naver.com>
2025-11-10 23:43:55 +0900

    🐛 fix: 공통 Layout ( 각 컴포넌트 title 적용 )

--- src/layouts/AppLayout.vue
+++ src/layouts/AppLayout.vue
@@ -1,12 +1,22 @@
 <template>
-  <div>
-    <MainHeader />
-    <main>
-      <router-view />
-    </main>
-  </div>
+  <MainHeader />
+  <main>
+    <h1>{{ title }}</h1>
+    <slot></slot>
+  </main>
 </template>
 
 <script setup>
 import MainHeader from '@/layouts/MainHeader.vue'
+import { ref, watchEffect } from 'vue'
+
+// 페이지 타이틀 받아오기
+const { pageName } = defineProps({ pageName: { type: String } })
+const title = ref('')
+
+// title 추적
+watchEffect(() => {
+  title.value = pageName
+  console.log('slot:', pageName)
+})
 </script>
