de89a343d83087987e8f48aa1a1c0b930d188ab2
ohiayame <0819aym001@gmail.com>
2025-11-11 04:22:31 +0900

    🔧 chore: rename

--- src/stores/professor.js
+++ src/stores/professor.js
@@ -1,23 +1,23 @@
-import { getProfessors } from '@/api/authApi';
+import { getProfessors } from '@/api/adminApi'
 import { defineStore } from 'pinia'
 
 // 교수 명단
 export const useProfessorStore = defineStore('professors', {
   state: () => ({
-    professors: []
+    professors: [],
   }),
   actions: {
     // ----------------------- professors -----------------------
     // [ set ] : professors 세팅
     async setProfessors() {
-      this.professors = await getProfessors();
+      this.professors = await getProfessors()
     },
     // [ get ] : professors조회
     async getProfessors() {
       if (this.professors.length <= 0) {
         await this.setProfessors()
       }
-      console.log(this.professors);
+      console.log(this.professors)
       return this.professors
     },
 
@@ -27,8 +27,8 @@ export const useProfessorStore = defineStore('professors', {
         await this.setProfessors()
       }
       const prfRow = this.professors.filter((prf) => prf.name == name)
-      // console.log("store", prfRow[0].user_id);
+      // console.log('store', prfRow[0].user_id)
       return prfRow[0].user_id
     },
-  }
+  },
 })
