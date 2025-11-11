f443040369b6d024ba6caa5c312b10714360063a
Kwon_Hyuk_il <gurdlf320@naver.com>
2025-11-11 21:54:19 +0900

    🐛 fix: api 요청 방식 수정

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
@@ -15,4 +13,15 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+
+  server: {
+    proxy: {
+      // '/api'로 시작하는 모든 요청을
+      // 'http://localhost:3000' (백엔드 서버)로 대신 보냄
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+      }
+    }
+  }
 })
