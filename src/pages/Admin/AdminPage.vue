2897fcf4a7d0bf1db91d67f9c2f7ecc92f94617b
ohiayame <0819aym001@gmail.com>
2025-11-11 10:26:14 +0900

    🐛 fix: 권한 추가

--- src/pages/Admin/AdminPage.vue
+++ src/pages/Admin/AdminPage.vue
@@ -1,13 +1,23 @@
 <script setup>
-import AppLayout from '@/layouts/AppLayout.vue';
-import Menubar from './Menubar.vue';
+import AppLayout from '@/layouts/AppLayout.vue'
+import Menubar from './Menubar.vue'
+import { ref, watch } from 'vue'
+import { useUserStore } from '@/stores/user'
+const Ustore = useUserStore()
+const isAdmin = ref(null) // 학생인지
 
+// 사용자 정보 확인
+watch(
+  () => {
+    isAdmin.value = Ustore.userInfo.role_type == 'admin' ? true : false
+    // console.log(isStudent.value)
+  },
+  { immediate: true },
+)
 </script>
 
 <template>
-  <AppLayout pageName="AdminPage">
-
-  <Menubar />
-
+  <AppLayout pageName="AdminPage" v-if="isAdmin">
+    <Menubar />
   </AppLayout>
 </template>
