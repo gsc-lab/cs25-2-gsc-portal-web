03e26871dffff29d8c42f05ec4de0ce21dab4fcf
Kwon_Hyuk_il <gurdlf320@naver.com>
2025-11-20 00:48:03 +0900

    🔧 chore: 소문자로 변경

--- src/api/apiCourse.js
+++ src/api/apiCourse.js
@@ -1,8 +1,9 @@
 import api from "./apiClient";
 
-// ============= 모든 과목 데이터 조회 API 요청 ==============
+// ============= 학년별 과목 조회 API 요청 ==============
 
 export const getCourse = async () => {
-  const Course = await api.get('/notices/form/courses')
-  return Course.data
+  const course = await api.get('/notices/form/courses/')
+
+  return course.data
 }
