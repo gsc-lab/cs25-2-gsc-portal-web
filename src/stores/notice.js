0b87cedc6d24e820cf10cfd6615035e5222c83fc
Kwon_Hyuk_il <gurdlf320@naver.com>
2025-11-23 00:18:45 +0900

    ♻️ refactor: 공지사항 조건에 맞는 params 알고리즘 추가

--- src/stores/notice.js
+++ src/stores/notice.js
@@ -83,7 +83,7 @@ export const useNoticeStore = defineStore('Notice', () => {
       const response = await getNotice(params)
 
       noticeList.value = response.notices
-      totalCount.value = response.total_count
+      totalCount.value = response.total
     } catch (err) {
       error.value = err
       console.error('공지사항 조회 실패', err)
@@ -115,80 +115,6 @@ export const useNoticeStore = defineStore('Notice', () => {
     }
     return paramVal
   }
-
-  // // 전체 공지사항 조회
-  // async function fetchNotice() {
-  //   isLoading.value = true
-
-  //   try {
-
-  //     const params = {
-  //       page: page.value,
-  //       size: 10,
-  //       keyword: keyword.value,
-  //       target: targetSelect.value === '전체' ? null : targetSelect.value
-  //     }
-
-
-  //     const response = await getNotice()
-  //     noticeList.value = response.notices
-  //   } catch (err) {
-  //     error.value = err
-  //     console.error("전체 공지사항을 조회 실패", err)
-  //   } finally {
-  //     isLoading.value = false
-  //   }
-  // }
-
-  // // 공지사항 필터링
-  // const filterNotices = computed(() => {
-  //   return noticeList.value.filter((notice) => {
-  //     const target = targetSelect.value;
-  //     const type = courseTypeSelect.value;
-  //     const selectedCourseId = courseSelect.value;
-
-  //     // 선택된 과목 id가 선택디고 공지사항의 과목 id 가 선택한 과목 id 와 일치하지 않을 경우 false
-  //     if (selectedCourseId && notice.course_id !== selectedCourseId) {
-  //       return false;
-  //     }
-
-  //     // 공지사항 타겟이 전체일 경우
-  //     if (target === '전체') {
-  //       // 공지사항의 과목 id 가 존재하지 않고 과목 타입이 general 이며, targets의 값이 빈 배열일 경우
-  //       return !notice.course_id && notice.course_type === 'general' && notice.targets.length === 0;
-  //     }
-
-  //     // course 안에는 courseMap 에 있는 course_id 값과 일치하는 공지사항의 course_id 를 가져와서 저장
-  //     // 존재하지 않을 경우 undifined
-  //     const course = courseMap.value.get(notice.course_id);
-
-  //     // 3. [1, 2, 3 학년] 안에 선택된 target에 해당하면 통과
-  //     if (['1', '2', '3'].includes(target)) {
-
-  //       // ( course 가 존재 하며, 과목의 학년과 선택한 학년의 값, 과목의 타입이 선택한 타입과 일치여부 확인)
-  //       const isCourse =
-  //         course &&
-  //         course.grade_id === target &&
-  //         course.course_type === type;
-
-  //       const isTarget =
-  //         notice.targets &&
-  //         notice.targets.some((tar) => tar.grade_id === target) &&
-  //         notice.course_type === type;
-
-  //       return isCourse || isTarget;
-  //     }
-
-
-  //     if (['special', 'korean'].includes(target)) {
-
-  //       // 1. 공지사항의 타입이 'special'이나 'korean'인지 확인
-  //       if (notice.course_type !== target) return false;
-
-  //       return notice.targets?.some(t => t.class_id && t.class_id.includes(type));
-  //     }
-  //   });
-  // });
 
   return {
     noticeList,
